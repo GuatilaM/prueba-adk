b946180d0b00d8018dcd9d80c179eb6437b7b6fa
Diego Rojas <guatilam@gmail.com>
2025-10-29 17:45:57 -0500

    feat: crea agente paralelizador y agente que usa solo emojis
    agrega utilidad para leer archivos de una carpeta de instrucciones

--- agente_prueba/agent.py
+++ agente_prueba/agent.py
@@ -1,8 +1,28 @@
 from google.adk.agents.llm_agent import Agent
+from google.adk.agents import ParallelAgent
 
-root_agent = Agent(
+from agente_prueba.utils import leer_instrucciones
+
+normal_agent = Agent(
     model='gemini-2.5-flash',
-    name='root_agent',
+    name='normal_agent',
     description='Un asistente presto a ayudar e informar con datos ambientales de Bogotá',
-    instruction='Responder las preguntas y solicitudes de los usuarios sobre datos ambientales dentro de las posibilidades de tu conocimiento',
+    instruction=leer_instrucciones(),
 )
+
+emoji_agent = Agent(
+    model='gemini-2.5-flash',
+    name='EmojiInterpretingAgent',
+    description='Recibe la solicitud del usuario y retorna una interpretación con sólo emojis',
+    instruction=leer_instrucciones("ins_emoji_agent.txt"),
+)
+
+parallel_agent = ParallelAgent(
+    name='ParallelInstructionAgent',
+    description='Corre múltiples agentes en paralelo.',
+    sub_agents=[normal_agent, emoji_agent],
+)
+
+# pendiente un agente integrador
+
+root_agent = parallel_agent
